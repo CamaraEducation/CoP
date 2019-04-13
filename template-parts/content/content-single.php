161ac5cd10007c12611250f463a7c28d39c284a6
unknown <tesfayegetenet@camara.org>
2019-04-13 03:54:13 -0700

    changes

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -60,7 +60,9 @@ if($leveltype = "Beginner"){}elseif ($leveltype= "Intermediate") {
 <?php 
 $s = getPostTerms($post->ID,'level');
 echo  $s;
-if($s = "Beginner"){}elseif ($s= "Intermediate") {
+if($s = "Beginner"){
+	
+}elseif ($s= "Intermediate") {
 	# code...
 }elseif ($s="Advance" ) {
 	# code...
