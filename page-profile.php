d550f632cac74a4ecc103bbf0ba55d3a2f5b8765
tesfayegetenet@camara.org <tesfayegetenet@camara.org>
2019-04-25 01:51:44 -0700

    Code oreder on profile page

--- page-profile.php
+++ page-profile.php
@@ -2,12 +2,9 @@
 get_header();?>
 <section id="hero">
 	<div class="hero-container">
-
 		<?php
 		if ( is_user_logged_in() ) {
-
 			?>
-
 			<div class="col-xs-2 col-centered mx-2">
 				<span><img src="<?php echo get_avatar_url($current_user->ID); ?>" class="img-responsive rounded-circle" width="125" height="125" alt="COP"></span>
 			</div>
@@ -20,13 +17,9 @@ get_header();?>
 					<span class="badge badge-light float-left" style="background-color: #B4B4B4; opacity: 0.5; color: #1d1d1d;">
 						<?php echo $current_user_pathway_name  ?></span>
 					</div>
-
 				<?php } else { ?>
-
 					"why are you there?"
-
 				<?php } ?>
-
 			</div>
 		</section>
 		<section>
@@ -49,7 +42,6 @@ get_header();?>
 				<h2 class="headtitle">Email Preference </h2>
 				<hr>
 			</div>
-
 			<div class="container">
 				<div class="row">
 					<div class="col-md-3">
@@ -66,59 +58,22 @@ get_header();?>
 					</div>
 					<div class="col-md-9">
 						<div class="row">
-
 							<div class="row">
-
 								<div class="card ml-2 mt-2" style="width: 700px; background: #FFFFFF;box-shadow: 0px 3px 5px rgba(25, 70, 93, 0.05);border-radius: 10px;border-bottom:30px;">
 									<div class="card-body">
-										<label style="width: 488px;
-										height: 36px;
-										left: 492px;
-										top: 812px;
-
-										font-family: Lato;
-										font-style: normal;
-										font-weight: bold;
-										font-size: 24px;
-										line-height: 36px;
-
-										color: #323F4B;">The types of programmes and activities I run are</label></br>
+										<label style="width: 488px;height: 36px;left: 492px;top: 812px;font-family: Lato;font-style: normal;font-weight: bold;font-size: 24px;line-height: 36px;color: #323F4B;">The types of programmes and activities I run are</label></br>
 										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 											tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 											quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 										consequat. </p>
-										<label style="width: 488px;
-										height: 36px;
-										left: 492px;
-										top: 812px;
-
-										font-family: Lato;
-										font-style: normal;
-										font-weight: bold;
-										font-size: 24px;
-										line-height: 36px;
-
-										color: #323F4B;">I'm interested in digital youth work because</label><br/>
-										<p style="width: 682px;
-										height: 48px;
-										left: 492px;
-										top: 712px;
-
-										font-family: Lato;
-										font-style: normal;
-										font-weight: normal;
-										font-size: 16px;
-										line-height: 24px;
-
-										color: #323F4B;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										<label style="width: 488px;height: 36px;left: 492px;top: 812px;font-family: Lato;font-style: normal;font-weight: bold;font-size: 24px;line-height: 36px;color: #323F4B;">I'm interested in digital youth work because</label><br/>
+										<p style="width: 682px;height: 48px;left: 492px;top: 712px;font-family: Lato;font-style: normal;font-weight: normal;font-size: 16px;line-height: 24px;color: #323F4B;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 										tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 										quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 									consequat.</p>
 								</div>
 							</div>
-
 						</div>
-
 					</div>
 					<h2 style="width: 348px; height: 36px; left: 432px; top: 1120px; font-family: Lato; font-style: normal; font-weight: bold; font-size: 24px; line-height: 36px; color: #323F4B;">Community Contributor Archive</h2>
 					<hr>
@@ -132,7 +87,6 @@ get_header();?>
 							<span class="badge badge-primary" style="background-color:<?php echo $current_pathway_color;?>"> Circuits</span>
 
 							<span class="badge badge-info" style="outline: 1px solid <?php echo $current_pathway_color;?>;color:<?php echo $current_pathway_color;?>;background-color:#ffffff"> TOOL</span>
-
 							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 							<img src="<?php echo get_template_directory_uri();  ?>/images/level.png" class="img-responsive" with="10px"  alt="COP">
 							<?php //echo getPostTerms($post->ID,'level'); ?>
